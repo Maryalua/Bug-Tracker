ba6a6ee708ed8cd91f87692ed75bc87aafce0095
Maryalua Meyer <maryaluameyer@yahoo.com.br>
2020-11-04 21:14:54 -0300

    Primeira versão - Modelos

---
+++ 3.0.Dashboard Modelo 1/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '../2.Login/node_modules/@material-ui/core/Typography';
+
+export default function Title(props) {
+  return (
+    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      {props.children}
+    </Typography>
+  );
+}
+
+Title.propTypes = {
+  children: PropTypes.node,
+};
